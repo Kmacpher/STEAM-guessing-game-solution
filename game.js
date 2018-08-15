c8be62cc4ad4e9266b3acd8916185b40e4eff192
Karen MacPherson <karen@fullstackacademy.com>
2018-08-15 09:25:39 -0400

    no docs

--- game.js
+++ game.js
@@ -1,12 +1,3 @@
-// isValidGuess takes the player's integer guess as an argument and checks to make sure that the argument is a number, is less than or equal to 100, and is greater than 0.
-// isValidGuess will return true is the guess is valid, and false if it is not.
-function isValidGuess(playersGuess) {
-    if (typeof playersGuess !== 'number' || playersGuess < 1 || playersGuess > 100) {
-        return false;
-    } else return true;
-}
-
-
 // isWinningGuess takes the player's guess as an argument and will return true if the guess is equal to the winning number. It will return false if the guess is not equal to the winning number.
 function isWinningGuess(playersGuess, winningNumber) {
     return playersGuess === winningNumber;
@@ -22,7 +13,7 @@ function isPastGuess(playersGuess, pastGuesses) {
 // addWrongGuessToPastGuesses adds an incorrect guess to the array of pastGuesses and displays that guess on the screen.
 function addWrongGuessToPastGuesses(playersGuess, pastGuesses) {
     pastGuesses.push(playersGuess);
-    document.querySelector(`#guess-list li:nth-child(${ pastGuesses.length + 1})`).textContent = playersGuess;
+    document.querySelector(`#guess-list li:nth-child(${ pastGuesses.length})`).textContent = playersGuess;
 }
 
 // isGameOver take the array pastGuesses as an argument and returns true if pastGuesses contains 5 or more integers. This means that the user has guessed incorrectly 5 times.
@@ -47,7 +38,7 @@ function updateText(text) {
 
 // youWin calls the updateText function, and passes in a string that shows the player that they won!
 function youWin() {
-    updateText('Congratulations, you won!')
+    updateText('Congratulations, you won! Refresh the page to play again!')
 }
 
 // youLose calls the updateText function, and passes in a string that shows the player that they lost and will need to refresh the page to play again.
@@ -59,21 +50,43 @@ function youLose() {
 // checkGuess takes the playersGuess, winningNumber, and array of pastGuesses as arguments
 // using these three values, use the above functions to write the behavior of the game. Reference the workshop docs for hints!
 function checkGuess(playersGuess, winningNumber, pastGuesses) {
-    console.log('Player\'s guess', playersGuess)
-    console.log('Winning Number', winningNumber)
-    // YOUR CODE HERE
+
+    if (isWinningGuess(playersGuess, winningNumber)) {
+        youWin();
+    } else {
+        if (isPastGuess(playersGuess, pastGuesses)) {
+            updateText('You have guessed that number already! Guess a different number.');
+        } else {
+            addWrongGuessToPastGuesses(playersGuess, pastGuesses);
+            if(isGameOver(pastGuesses)) {
+                youLose();
+            } else {
+                const text = howClose(playersGuess, winningNumber);
+                updateText(text);
+            }
+        }
+    }
 }
 
 function playGame() {
+    // Generate a winningNumber randomly
     const winningNumber = Math.ceil(Math.random() * 100);
+    // we start with no past, wrong guesses
     const pastGuesses = [];
+
+    // We are grabbing the button from our html
     const button = document.querySelector('button');
-    console.log(button)
-    button.addEventListener('click', () => {
+
+    // We are listening for when the use clicks on our button.
+    // When they click, we will check in the input field to see if they have guessed a number. Then we will run the function `checkGuess`, and give it the player's guess, the winning number, and the empty array of guesses!
+    button.addEventListener('click', function() {
+
         const playersGuess = +document.querySelector('input').value;
+        document.querySelector('input').value = 0;
         checkGuess(playersGuess, winningNumber, pastGuesses)
     })
 
 }
 
+// start up the game!
 playGame();
